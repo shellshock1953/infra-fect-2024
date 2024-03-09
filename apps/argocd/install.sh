10f26b5c6a0a569a43bd5820a15f083b24bd0dba
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-03-09 11:53:41 +0200

    install.sh

---
+++ apps/argocd/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SYSENV="${1}"
+
+if [ -z "${SYSENV}" ]; then
+  echo "Please provide the SYSENV name: stage/prod"
+  exit 1
+fi
+
+echo "Installing fect-${SYSENV}"
+sleep 4
+
+helm upgrade --install "fect-${SYSENV}" ./ --values "${SYSENV}".yaml
